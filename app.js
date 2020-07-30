ee2e230b9f787957833f513dec0cadb6a554e19b
Suyun514 <Suyun514@qq.com>
2020-07-30 11:02:24 +0800

    Improve the accuracy of lastUpdateTime

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const moment = require('moment');
 const config = require('./config.json');
 
 const contests = [];
-const lastUpdateTime = moment();
 
 Promise.all(fs.readdirSync('.').filter((file) => file.endsWith('.js') && file != 'app.js').map((file) => {
     const oj = require(`./${file}`);
     return oj.contests.then(list => list.forEach((el) => contests.push([oj.name, ...el])));
 })).then(() => {
-
     contests.sort((a, b) => a[2] - b[2]);
 
     const app = new Koa;
     app.use(cors());
+
+    const lastUpdateTime = moment();
     app.use(async (ctx) => {
         ctx.body = {
             'status': 'OK',
