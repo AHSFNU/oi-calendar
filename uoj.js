ac16bfcb26955f254b5f7e805bf3d4e45d8eb6a2
hotwords123 <hotwords123@gmail.com>
2020-08-10 20:01:34 +0800

    Add Universal OJ

---
+++ uoj.js
@@ -0,0 +1,38 @@
+const fetch = require('node-fetch');
+const moment = require('moment');
+const cheerio = require('cheerio');
+
+module.exports.name = 'UOJ';
+
+module.exports.contests = fetch('https://uoj.ac/contests').then(res => res.text()).then(body => {
+    let contests = [];
+
+    const $ = cheerio.load(body);
+
+    $('.uoj-content>.table-responsive:first-of-type>table>tbody>tr').each((index, tr) => {
+        let meta = {};
+
+        $(tr).find('td').each((index, td) => {
+            const $td = $(td);
+            switch (index) {
+                case 0: {
+                  let $a = $td.children('a');
+                  meta.id = parseInt($a.attr('href').split('/').pop());
+                  meta.name = $a.text();
+                  meta.url = `https://uoj.ac/contest/${meta.id}`;
+                  break;
+                }
+                case 1: meta.startTime = moment($td.find('a').text()); break;
+                case 2: {
+                  let hours = $td.text().match(/^([\d.]+) .+$/)[1];
+                  meta.endTime = moment(meta.startTime).add(hours, 'h');
+                  break;
+                }
+            }
+        });
+
+        contests.push(meta);
+    });
+
+    return contests;
+});
